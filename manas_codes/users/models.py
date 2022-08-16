0a81806f19a43b6d4b5994dea548130f74597692
Manas <manasm.dev@gmail.com>
2022-08-16 22:02:57 +1000

    cleared bug

--- manas_codes/users/models.py
+++ manas_codes/users/models.py
@@ -1,5 +1,4 @@
 from typing import Optional
-from typing_extensions import Required
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import CharField
